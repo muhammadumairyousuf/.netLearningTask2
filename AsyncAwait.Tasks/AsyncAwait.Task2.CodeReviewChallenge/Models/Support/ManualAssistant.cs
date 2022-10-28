5b9aa2621942d17604d9cd144b20544ee6d42a99
Muhammad_Yousuf <Muhammad_Yousuf@EPPLKRAW0436>
2022-10-28 16:03:20 +0200

    commit changes

--- AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Models/Support/ManualAssistant.cs
+++ AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Models/Support/ManualAssistant.cs
@@ -19,16 +19,23 @@ public class ManualAssistant : IAssistant
     {
         try
         {
-            var t = _supportService.RegisterSupportRequestAsync(requestInfo);
-            Console.WriteLine(t.Status); // this is for debugging purposes
-            Thread.Sleep(5000); // this is just to be sure that the request is registered
-            return await _supportService.GetSupportInfoAsync(requestInfo)
-                .ConfigureAwait(false);
+
+
+            await Task.Run(() => {
+                _supportService.RegisterSupportRequestAsync(requestInfo);
+            }).ContinueWith((t1) => {
+                return _supportService.GetSupportInfoAsync(requestInfo);
+            });
+            // Console.WriteLine(t.Status); // this is for debugging purposes
+            // Thread.Sleep(5000); // this is just to be sure that the request is registered
+            //return await _supportService.GetSupportInfoAsync(requestInfo)
+            //    .ConfigureAwait(false);
+            return null;
         }
         catch (HttpRequestException ex)
         {
-            return await Task.Run(async () =>
-                await Task.FromResult($"Failed to register assistance request. Please try later. {ex.Message}"));
+            //return await Task.Run(async () =>
+            return await Task.FromResult($"Failed to register assistance request. Please try later. {ex.Message}");
         }
     }
 }
